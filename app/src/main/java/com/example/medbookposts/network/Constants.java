2d8605f16212a359e2c6c8c56dced7f196aff5af
MichaelOdera <michogelira@gmail.com>
2020-08-04 03:37:10 +0300

    did away with the unused variable for user id in the file Constants.java

--- app/src/main/java/com/example/medbookposts/network/Constants.java
+++ app/src/main/java/com/example/medbookposts/network/Constants.java
@@ -3,5 +3,4 @@ package com.example.medbookposts.network;
 public class Constants {
 
     public static final String TYPICODE_URL = "https://jsonplaceholder.typicode.com/";
-    public String postIdUpdate = "";
 }
